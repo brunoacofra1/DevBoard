72763e8541df440fbf1ab5017b373ff0ec4521df
Bruno açofra <brunoacofra1@gmail.com>
2026-08-29 21:32:46 -0300

    Iniciando as rotas

--- usuario/urls.py
+++ usuario/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
 from . import views
 
+app_name = "usuarios"
+
 urlpatterns =[
-    path('',views.usuario_index)
+    path('',views.usuario_index,name="index"),
+    path('adicionar',views.usuario_adicionar,name="adicionar")
 ]
--- usuario/views.py
+++ usuario/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-
-def usuario_index(response):
-    return HttpResponse("Aqui é o index do usuario")
+def usuario_index(request):
+    return render(request, 'usuarios/indexUser.html')
+def usuario_adicionar(request):
+    return render(request,'usuarios/adicionarUser.html')
